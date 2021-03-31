7d65711b7b3e8513f6b78be822d56143a376260d
matt08-prog <58103083+matt08-prog@users.noreply.github.com>
2021-03-31 00:09:51 -0600

    added score

---
+++ text.js
@@ -0,0 +1,40 @@
+class Text {
+    constructor(scene, camera, THREE) {
+        this.THREE = THREE
+        this.scene = scene
+        this.initScene()
+    }
+
+    initScene() {
+        // Load Font
+        const loader = new this.THREE.FontLoader();
+
+        loader.load( 'libs/fonts/PressStart2P-Regular.ttf', function ( font ) {
+
+            console.log("loaded font")
+            const geometry = new this.THREE.TextGeometry( 'Hello three.js!', {
+                font: font,
+                size: 80,
+                height: 5,
+                curveSegments: 12,
+                bevelEnabled: true,
+                bevelThickness: 10,
+                bevelSize: 8,
+                bevelOffset: 0,
+                bevelSegments: 5
+            } );
+
+            const material = new this.THREE.MeshPhongMaterial( { color: 0xff38b9, fog: false} ) // front
+
+            const mesh = new this.THREE.Mesh(geometry, material)
+            this.scene.add(mesh)
+
+        } );
+    }
+
+    update(){
+
+    }
+}
+
+export { Text }
