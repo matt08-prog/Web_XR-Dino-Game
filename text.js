39d5fd8bec389ead53fa00f9bb1f7c424bbaad63
matt08-prog <58103083+matt08-prog@users.noreply.github.com>
2021-04-01 01:08:04 -0600

    added score

--- text.js
+++ text.js
@@ -10,40 +10,48 @@ class Text {
         console.log("loading font currently")
 
         const loader = new this.THREE.FontLoader();
-        
-        var request = new XMLHttpRequest();
-        request.open("GET", "libs/fonts/PressStart2P-Regular.json", false);
-        request.send(null)
-        var json = JSON.parse(request.responseText);
+
+        // var request = new XMLHttpRequest();
+        // request.open("GET", "libs/fonts/PressStart2P-Regular.json", false);
+        // request.send(null)
+        // var json = JSON.parse(request.responseText);
+        const self = this
+        $.ajax({
+            async: true,
+            type: "GET",
+            url: "libs/fonts/PressStart2P-Regular.json",
+            contentType: "application/json",
+            data: JSON.stringify({ ParameterName: paramValue }),
+         }).then((font) => {
+            loader.parse( json, function ( data ) {
+                try {
+                    console.log("loaded font")
+                    const geometry = new self.THREE.TextGeometry( "Hello three.js!", {
+                        font: data.font,
+                        size: 80,
+                        height: 5,
+                    } );
+    
+                    geometry.computeBoundingBox()
+                    const material = new self.THREE.MeshPhongMaterial( { color: 0xff38b9, fog: false} ) // front
+    
+                    const centerOffset = - 0.5 * ( geometry.boundingBox.max.x - geometry.boundingBox.min.x );
+    
+                    const mesh = new self.THREE.Mesh(geometry, material)
+                    
+                    mesh.position.x = centerOffset
+    
+                    self.scene.add(mesh)
+                } catch (e) {
+                    console.error(e)
+                }
+            } );
+         });
+
         //alert (my_JSON_object.result[0]);
 
         
         //const json = JSON.parse(  )
-        
-        const self = this
-        loader.parse( json, function ( font ) {
-            try {
-                console.log("loaded font")
-                const geometry = new self.THREE.TextGeometry( "Hello three.js!", {
-                    font: font,
-                    size: 80,
-                    height: 5,
-                } );
-
-                geometry.computeBoundingBox()
-                const material = new self.THREE.MeshPhongMaterial( { color: 0xff38b9, fog: false} ) // front
-
-                const centerOffset = - 0.5 * ( geometry.boundingBox.max.x - geometry.boundingBox.min.x );
-
-                const mesh = new self.THREE.Mesh(geometry, material)
-                
-                mesh.position.x = centerOffset
-
-                self.scene.add(mesh)
-            } catch (e) {
-                console.error(e)
-            }
-        } );
     }
 
     update(){
