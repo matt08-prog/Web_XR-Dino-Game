a4229252ae00e1962cb9da0f973aad449ce94917
matt08-prog <58103083+matt08-prog@users.noreply.github.com>
2021-04-01 00:46:20 -0600

    added score

--- text.js
+++ text.js
@@ -11,20 +11,22 @@ class Text {
         const loader = new this.THREE.FontLoader();
         const self = this
         loader.load( 'libs/fonts/PressStart2P-Regular.json', function ( font ) {
+            try {
+                console.log("loaded font")
+                const geometry = new self.THREE.TextGeometry( "Hello three.js!", {
+                    font: font,
+                    size: 80,
+                    height: 5,
+                } );
 
-            console.log("loaded font")
-            const geometry = new self.THREE.TextGeometry( 'Hello three.js!', {
-                font: font,
-                size: 80,
-                height: 5,
-            } );
+                geometry.computeBoundingBox()
+                const material = new self.THREE.MeshPhongMaterial( { color: 0xff38b9, fog: false} ) // front
 
-            geometry.computeBoundingBox()
-            const material = new self.THREE.MeshPhongMaterial( { color: 0xff38b9, fog: false} ) // front
-
-            const mesh = new self.THREE.Mesh(geometry, material)
-            self.scene.add(mesh)
-
+                const mesh = new self.THREE.Mesh(geometry, material)
+                self.scene.add(mesh)
+            } catch (e) {
+                console.error(e)
+            }
         } );
     }
 
